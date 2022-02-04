c910d4655a65c34718f8eec44bd9f13c90613a41
leenrichards <leenrichards@yahoo.com>
2022-02-04 07:02:13 -0800

    Allowed Origins localhost:1234

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use Cors
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
 app.use(cors({
     origin: (origin, callback) => {
