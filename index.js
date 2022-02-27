c8ff4fa65cd3e5394c0c35348b6c6d76ab7084f4
leenrichards <leenrichards@yahoo.com>
2022-02-27 08:19:42 -0800

    Added AllowedOrigins herokuapp

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use Cors
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://lynnflix.herokuapp.com/'];
 
 app.use(cors({
     origin: (origin, callback) => {
